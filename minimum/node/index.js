1c6b209fb625ba6815f994c619695f1475c92411
Raphael Sizemoore <morgan@morganmakes.art>
2023-09-02 00:07:40 -0700

    changed folder structure for nodejs so it's more than just the one file

---
+++ minimum/node/index.js
@@ -0,0 +1,48 @@
+require("dotenv").config()
+class Activity {
+    constructor() {
+        this.jsonProps={"@context" : ["https://www.w3.org/ns/activitystreams"]}
+    }
+    add(props) {
+        for (let prop in props)
+        this.jsonProps[prop] = props[prop];
+    }
+    json() {
+        return JSON.stringify(this.jsonProps)
+    }
+}
+class Outbox extends Activity {
+    constructor(preferredUsername) {
+
+    }
+}
+class Actor extends Activity {
+    constructor(preferredUsername,type,{name,summary,icon}) {
+        super()
+        let id = process.env.DOMAIN + "/u/" + preferredUsername;
+        let add = {preferredUsername,id,name,summary,icon};
+        switch (type.toLowerCase()) {
+            case "person":
+                add.type = "Person";
+                break;
+            case "group":
+                add.type = "Group";
+                break;
+            case "organization":
+                add.type = "Organization";
+                break;
+            default:
+                throw 'Type must be "Person", "Group", or "Organization' 
+        }
+        for (let collection of ["following","followers","liked","inbox","outbox"]) {
+            let colId = id + "/" + collection
+            add[collection] = colId
+            this[collection] = Activity()
+            this[collection].add({colId})
+        }
+        this.add(add)
+    }
+}
+
+let a = new Actor("mergerg");
+console.log(a.json())
